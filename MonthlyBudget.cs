0fe0dbf82249355260b5eaa6d32ff3153f68504c
severedsolo <evil.penguin@live.co.uk>
2016-08-29 08:59:49 +0100

    Added Wage Calculator

--- MonthlyBudget.cs
+++ MonthlyBudget.cs
@@ -1,5 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using System.Linq;
+using System.Text;
 
 namespace severedsolo
 {
@@ -13,6 +17,8 @@ namespace severedsolo
         private float budget = 0.0f;
         private double funds = 0.0f;
         int multiplier = 2500;
+        int AvailableWages = 5000;
+        int AssignedWages = 10000;
         string SavedFile = KSPUtil.ApplicationRootPath + "/saves/" + HighLogic.SaveFolder + "/MonthlyBudget.cfg";
 
         private void Budget()
@@ -26,6 +32,7 @@ namespace severedsolo
                     Rep = Reputation.CurrentRep;
 
                     budget = Rep * multiplier;
+                    budget = budget - CrewBudget();
                     if (budget <= multiplier)
                     {
                         budget = multiplier;
@@ -74,9 +81,11 @@ namespace severedsolo
                 ConfigNode saved = ConfigNode.Load(SavedFile);
                 if (saved != null)
                 {
-                    double.TryParse(saved.GetValue("TimeElapsed"), out LastUpdate);
+                    double.TryParse(saved.GetValue("TimeElapsed (DO NOT CHANGE)"), out LastUpdate);
                     int.TryParse(saved.GetValue("Multiplier"), out multiplier);
                     int.TryParse(saved.GetValue("Budget Interval (Kerbin Days)"), out friendlyInterval);
+                    int.TryParse(saved.GetValue("Unassigned Kerbals wage"), out AvailableWages);
+                    int.TryParse(saved.GetValue("Assigned Kerbals wage"), out AssignedWages);
                     Debug.Log("MonthlyBudgets: Loaded settings");
                     if (friendlyInterval != 30)
                     {
@@ -92,11 +101,44 @@ namespace severedsolo
         private void Save()
         {
             ConfigNode savedNode = new ConfigNode("BudgetSave");
-            savedNode.AddValue("TimeElapsed", LastUpdate);
+            savedNode.AddValue("TimeElapsed (DO NOT CHANGE)", LastUpdate);
             savedNode.AddValue("Multiplier", multiplier);
             savedNode.AddValue("Budget Inverval (Kerbin Days)", friendlyInterval);
+            savedNode.AddValue("Unassigned Kerbals wage", AvailableWages);
+            savedNode.AddValue("Assigned Kerbals wage", AssignedWages);
             savedNode.Save(SavedFile);
             Debug.Log("MonthlyBudgets: Saved game");
+        }
+
+        private int CrewBudget()
+        {
+            int Budget = 0;
+            IEnumerable<CrewItemContainer> AvailableCrew = GameObject.FindObjectsOfType<CrewItemContainer>().Where(x => x.GetCrewRef().rosterStatus == ProtoCrewMember.RosterStatus.Available);
+            IEnumerable<CrewItemContainer> AssignedCrew = GameObject.FindObjectsOfType<CrewItemContainer>().Where(x => x.GetCrewRef().rosterStatus == ProtoCrewMember.RosterStatus.Assigned);
+            int CrewCount = 0;
+            int AvailableBudget = 0;
+            int AssignedBudget = 0;
+            if (AvailableCrew != null)
+            {
+                
+                foreach (CrewItemContainer crew in AvailableCrew)
+                {
+                    CrewCount = CrewCount + 1;
+                }
+                AvailableBudget = CrewCount * AvailableWages;
+            }
+            CrewCount = 0;
+            if(AssignedCrew != null)
+            {
+                foreach (CrewItemContainer crew in AssignedCrew)
+                {
+                    CrewCount = CrewCount + 1;
+                }
+                AssignedBudget = CrewCount * AssignedWages;
+            }
+            Budget = AvailableBudget + AssignedBudget;
+            Debug.Log("MonthlyBudgets: Crew Budget is " + Budget);
+            return Budget;
         }
     }
     }

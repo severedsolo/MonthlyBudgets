ce75e620969e4cb92deaae8e9285e7a5c89340df
severedsolo <evil.penguin@live.co.uk>
2016-09-03 14:14:03 +0100

    Don't count flags, or asteroids

--- MonthlyBudget.cs
+++ MonthlyBudget.cs
@@ -110,7 +110,7 @@ namespace severedsolo
             IEnumerable<ProtoCrewMember> crew = HighLogic.CurrentGame.CrewRoster.Crew;
             int availableBudget = crew.Count(a => a.rosterStatus == ProtoCrewMember.RosterStatus.Available)*availableWages;
             int assignedBudget = crew.Count(a => a.rosterStatus == ProtoCrewMember.RosterStatus.Assigned)* assignedWages;
-            IEnumerable<Vessel> vessels = FlightGlobals.Vessels.Where(v => v.vesselType != VesselType.Debris);
+            IEnumerable<Vessel> vessels = FlightGlobals.Vessels.Where(v => v.vesselType != VesselType.Debris && v.vesselType != VesselType.Flag && v.vesselType != VesselType.SpaceObject && v.vesselType != VesselType.Unknown);
             int vesselBudget = vessels.Count() *vesselCost;
             int budget = availableBudget + assignedBudget + vesselBudget;
             Debug.Log("MonthlyBudgets: Expenses are " + budget);
